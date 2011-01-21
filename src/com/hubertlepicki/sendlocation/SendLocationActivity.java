668283ba8a86c47efd58578f7b475d9dca905a93
Hubert <hubert.lepicki@amberbit.com>
2011-01-21 21:31:13 +0100

    initial project

---
+++ src/com/hubertlepicki/sendlocation/SendLocationActivity.java
@@ -0,0 +1,15 @@
+package com.hubertlepicki.sendlocation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SendLocationActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
